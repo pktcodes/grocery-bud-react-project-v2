0fe2963e24b7f3b13159777aff58d5bccadbcfab
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-08-07 23:54:07 -0500

    Single Item

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,5 +1,26 @@
-const SingleItem = () => {
-  return <h2>Single Item</h2>;
+import { useState } from 'react';
+
+const SingleItem = ({ item }) => {
+  const [isChecked, setIsChecked] = useState(item.completed);
+
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        value={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <p
+        style={{
+          textDecoration: isChecked && 'line-through',
+          textTransform: 'capitalize',
+        }}
+      >
+        {item.name}
+      </p>
+      <button className="btn remove-btn">delete</button>
+    </div>
+  );
 };
 
 export default SingleItem;
